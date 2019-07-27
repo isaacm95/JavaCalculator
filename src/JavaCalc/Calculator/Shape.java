c384c41405f3ea657a5c4f3bd9cf1c053aa77242
isaacm <iesm2@alumni.cam.ac.uk>
2019-07-27 19:09:26 +0100

    Build project/test folders

---
+++ src/JavaCalc/Calculator/Shape.java
@@ -0,0 +1,8 @@
+package JavaCalc.Calculator;
+
+public class Shape {
+    public void printShape()
+    {
+        System.out.println("I like squares.");
+    }
+}
---
+++ src/JavaCalc/Main.java
@@ -0,0 +1,11 @@
+package JavaCalc;
+
+import JavaCalc.Calculator.Shape;
+
+public class Main {
+    public static void main (String[] args){
+        var newShape = new Shape();
+        newShape.printShape();
+        System.out.println("hello");
+    }
+}
